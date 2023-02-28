4392524cf93d3d4e56e9d5e15d2a1c402b15f912
= <=>
2023-02-28 11:34:34 -0300

    Teste Vue3

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+
+</script>
+
+<template>
+  <div class="container">
+    <div></div>
+    <div class="contador">
+      <button @click="incrementarContador">Incrementar</button>
+      <button @click="decrementarContador">Decrementar</button>
+      <p>Valor do contador é: {{ contador }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+button{
+  border: 1px 1px black;
+  outline:none; 
+  color: rgb(0, 0, 0);
+  background-color: rgb(255, 255, 255);
+}
+button:hover{
+  border-radius: 25%;
+}
+.container {
+  display: grid;
+  grid-template-rows: 3fr 1fr;
+  justify-items: center;}
+.contador{
+  display: grid;
+}
+</style>
